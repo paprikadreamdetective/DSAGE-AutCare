d1af7844fcc9ec217133848d322fcb98e37e78c9
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-02-09 02:20:43 -0600

    add functional calendar

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -2,24 +2,42 @@ import React, {useState} from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
-import Calendar from 'react-calendar';
 import { Person } from '../models/person.model'
 import cloud_icon from '../assets/cloud.png'
 
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import dayjs from "dayjs";
+import { CiCalendarDate } from "react-icons/ci";
+import "dayjs/locale/es"
+
+dayjs.locale("es");
 
 const Tab2: React.FC = () => {
-  
+  const [currentDate, setCurrentDate] = useState<Date>(dayjs("2023-12-19T12:00:00").toDate());
   const [people, setPeople] = useState<Person[]>([]);
-  const [date, setDate] = useState<Date | Date[]>(new Date());
+  const localizer = dayjsLocalizer(dayjs);
 
-  const onChange = (date: Date | null) => {
-    if (date !== null) {
-      setDate(date);
-    }
-    // Aquí puedes hacer algo con la fecha seleccionada, como cargar datos específicos, etc.
-  };
+  const events = [
+    {
+      start: dayjs("2023-12-18T12:00:00").toDate(),
+      end: dayjs("2023-12-18T13:00:00").toDate(),
+      data: {
+        x: 10,
+        title: "Evento 1",
+      },
+    },
+    {
+      start: dayjs("2023-12-23T12:00:00").toDate(),
+      end: dayjs("2023-12-24T12:00:00").toDate(),
+      data: {
+        title: "Evento 2",
+        x: 20,
+      },
+    },
+  ];
   
-  return (
+  {/*return (
     <IonPage>
       <IonContent fullscreen>
         <div className="description">
@@ -33,28 +51,70 @@ const Tab2: React.FC = () => {
               <p>Casual</p>
           </div>
         </div>
-        <div className="calendar-container">
-          <Calendar onChange={(value) => setDate(date)}
-            calendarType="US"
-            locale="en-US"
-            showNeighboringMonth
-            
         
-            
-            tileClassName={({ date, view }) => {
-              // Estilos del día seleccionado
-              if (date instanceof Date && date.toDateString() === new Date().toDateString()) {
-                return 'react-calendar__tile--now';
-              }
-              return '';
-            }}
-            prev2Label={null}
-            next2Label={null}
-            navigationAriaLabel="Calendar navigation"
-          />
-        </div>
       </IonContent>
     </IonPage>
+  );*/}
+
+  const handleNavigate = (newDate: Date) => {
+    console.log("New date:", newDate);
+    setCurrentDate(newDate);
+    // Aquí puedes hacer lo que necesites con la nueva fecha,
+    // como actualizar el estado para mostrar eventos del nuevo período.
+  };
+
+  const components = {
+    //event: (props) => {
+      event: (props: { event: { data: {  x: number, title: string; }; } }) => {
+      const { data } = props.event;
+      console.log(data);
+
+      if (data && data.x !== undefined && data.x > 15) {
+        return (
+          <div style={{ background: "red" }}>
+            <CiCalendarDate />
+            {props.event.data.title}
+          </div>
+        );
+      } else {
+        return (
+          <div style={{ background: "green" }}>
+            <CiCalendarDate />
+            {props.event.data.title}
+          </div>
+        );
+      }
+    },
+  };
+
+  return (
+    <div
+      style={{
+        height: "95vh",
+        width: "70vw",
+      }}
+    >
+      <Calendar
+        localizer={localizer}
+        events={events}
+        views={["month", "week", "day"]}
+        // view={"month"}
+        //date={dayjs("2023-12-19T12:00:00").toDate()}
+        date={currentDate}
+        toolbar={true}
+        defaultView="month"
+        onNavigate={handleNavigate}
+        //min={dayjs("2023-12-23T08:00:00").toDate()}
+       // max={dayjs("2023-12-23T18:00:00").toDate()}
+        formats={{
+          dayHeaderFormat: (date) => {
+            console.log(date);
+            return dayjs(date).format("DD/MM/YYYY");
+          },
+        }}
+        components={components}
+      />
+    </div>
   );
 };
 
