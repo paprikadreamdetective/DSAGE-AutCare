fbd715a021a777a6a31aa214d1626bc04608b72a
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-02-13 02:28:08 -0600

    login is up

--- src/components/mail-component.tsx
+++ src/components/mail-component.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { IonButton, IonInput, IonItem, IonLabel, IonList, IonLoading, IonNote } from '@ionic/react';
 import './mail-component.css'
 import { loginUser } from '../firebaseConfig';
+import { useHistory } from 'react-router-dom';
 
 // import {toast} from '../toast'
 import { error } from 'console';
@@ -10,9 +11,10 @@ function Email_input() {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [busy, setBusy] = useState<boolean>(false)
-
-  async function login() {
+  //const [busy, setBusy] = useState<boolean>(false)
+  const history = useHistory();
+  /*async function login() {
+    setBusy(true);
     const res = await loginUser(username, password)
     if(!res){
       // console.log(error)
@@ -22,6 +24,42 @@ function Email_input() {
 
     }
     // setBusy(false)
+  }*/
+
+  /*const [values, setValues] = useState(initialValues);
+
+
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setValues({...values, [name]: value})
+        
+    }*/
+    
+
+  const login = async (e: any) => {
+    //setBusy(true);
+    e.preventDefault();
+    try {
+      const response = await fetch('http://192.168.100.136:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nombre_usuario: username, auth_key: password })
+      });
+
+      if (response.ok) {
+        // Login successful, navigate to home
+        console.log('Inicio de sesión exitoso');
+        history.push('/home');
+      } else {
+        console.error('Login failed');
+        // Display an error message or handle the error as needed
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      // Display an error message or handle the error as needed
+    } 
   }
 
   return (
@@ -44,7 +82,7 @@ function Email_input() {
       </IonInput>
     </div>
     
-      <IonButton routerLink='/home' className='button-signin-log'> Login </IonButton>
+      <IonButton routerLink='/home' className='button-signin-log' onClick={login}> Login </IonButton>
       {/* <IonButton className='button-register' onClick={login}> Log In </IonButton> */}
    
 
